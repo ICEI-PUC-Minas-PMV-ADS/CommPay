0b5535a7985277894c6d86c0471d9ca30ef219c8
VictorKingma <victormkingma@gmail.com>
2023-04-25 20:45:51 -0300

    changes

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,4 +1,5 @@
 ﻿using Commpay.Models.Enums;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,7 +12,7 @@ namespace Commpay.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Preenchimento obrigatório!")]
-        [RegularExpression("^+~+[a-zA-Z]", ErrorMessage ="Apenas caracteres de A a Z são permitidos")]
+        [RegularExpression("^[a-zA-Z]+", ErrorMessage ="Apenas caracteres de A a Z são permitidos")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Preenchimento obrigatório!")]
--- Startup.cs
+++ Startup.cs
@@ -72,10 +72,15 @@ namespace Commpay
 
             app.UseEndpoints(endpoints =>
             {
-                //endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "home",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                ).RequireAuthorization();
+
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Usuarios}/{action=Login}/{id?}"
+                );
             });
         }
     }
