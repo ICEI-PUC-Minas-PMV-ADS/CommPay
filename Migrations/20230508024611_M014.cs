1931c03661db94bd90340c3fa415725934a5bcc5
Pablo Muriell Batista Alves <89876269+pablo-alves92@users.noreply.github.com>
2023-05-07 23:49:54 -0300

    Feat: Adicionando a Migration de mudança do tipo primitivo de Valor_Total na tabela Vendas.

---
+++ Migrations/20230508024611_M014.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Commpay.Migrations
+{
+    /// <inheritdoc />
+    public partial class M014 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Valor_Total",
+                table: "Venda",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Valor_Total",
+                table: "Venda",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
